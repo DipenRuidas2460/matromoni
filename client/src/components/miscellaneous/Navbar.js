5e4733f2d93ecc3698d474d8ded187cdf0661cd6
Dipen Ruidas <105585011+DipenRuidas2460@users.noreply.github.com>
2023-12-06 20:05:54 +0530

    today's-work-06-12-2023

--- client/src/components/miscellaneous/Navbar.js
+++ client/src/components/miscellaneous/Navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => {
     <div>
       <Header />
       <div className="navbar-con">
-        <div>
-          <p style={{ color: "#c2485d", fontSize: "18px" }}>Indian Diaspora</p>
-          <p style={{ color: "orange", fontSize: "15px" }}>MATRIMONY</p>
+        <div className="navbar-para-parent">
+          <p className="navbar-para-one">Indian Diaspora</p>
+          <p className="navbar-para-two">MATRIMONY</p>
         </div>
         <div className="navbar-con-1">
-          <p>Already a member?</p>
+          <p className="navbar-para-three">Already a member?</p>
           <button className="navbar-con-1-button">LOGIN</button>
         </div>
       </div>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -17,19 +17,14 @@ function HomePage() {
       <Navbar />
       <div>
         <img
-          src="https://www.shaadisquad.com/images/slider/1.jpg"
+          src="https://i.pinimg.com/originals/0a/53/17/0a5317e60c5b378846fe384e23671f6c.png"
           alt="shaadiPhoto"
-          style={{ objectFit: "cover" }}
+          style={{ objectFit: "cover", width:"100%"}}
         />
         <div className="co-ho">
-          <form
-            className="form-co-ho"
-          >
+          <form className="form-co-ho">
             <div className="form-main">
-              <label
-                htmlFor="inp1"
-                className="form-label-1"
-              >
+              <label htmlFor="inp1" className="form-label-1">
                 I'am looking for
               </label>
               <input type="text" className="looking-inp" id="inp1" />
@@ -45,34 +40,35 @@ function HomePage() {
               </div>
             </div>
             <div className="form-main">
-              <label
-                htmlFor="inp3"
-                className="form-label-3"
-              >
+              <label htmlFor="inp3" className="form-label-3">
                 Community
               </label>
               <input type="text" className="looking-inp" id="inp3" />
             </div>
             <div className="form-main">
-              <label
-                htmlFor="inp4"
-                className="form-label-4"
-              >
+              <label htmlFor="inp4" className="form-label-4">
                 Living in
               </label>
               <input type="text" className="looking-inp" id="inp4" />
             </div>
-            <button
-              type="button"
-              className="btn btn-info"
-            >
+            <button type="button" className="btn btn-info">
               Let's Begin
             </button>
           </form>
         </div>
       </div>
       <div className="parent-box">
-        <div className="home-conten"></div>
+        <div className="home-conten">
+          <div className="home-con-heading">
+            Why Choose Indian Diaspora
+          </div>
+          <div className="diapo-parent">
+            <div className="diapo-boxes">Global Diaspora Indian Roots</div>
+            <div className="diapo-boxes">100% Verified Profiles</div>
+            <div className="diapo-boxes">Review your maches for free</div>
+            <div className="diapo-boxes">Voice & Video Calling Features</div>
+          </div>
+        </div>
         <div className="home-conten-box1"></div>
         <div className="home-conten-box2"></div>
         <div className="home-conten-box3"></div>
